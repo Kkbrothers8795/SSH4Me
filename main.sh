dcd7808cc1e9f84289e07ec22d45d817cdef07d0
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-07-21 20:06:34 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-docker build -t ssh4me --build-arg AUTH_TOKEN=1uD2PPU1yFDWN1TRZKl8Li5AYOH_2t7GxZk5F4r4DkZPB25m7 --build-arg PASSWORD=rajughoda69
+docker build -t ssh4me --build-arg AUTH_TOKEN=2jMtT8iPA3wJvCCFiuuc3lFNnEg_51MgegPoTJixrmJMzaNBn --build-arg PASSWORD=rajughoda69
 docker run ssh4me
